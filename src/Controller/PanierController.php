7d936b44dd87409a194d3417f8b3e08ef5ef855e
Alex <tgvalex@hotmail.fr>
2023-02-16 11:56:33 +0100

    Add route to show cart

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleStockRepository;
+use App\Repository\PanierRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class PanierController extends AbstractController
 {
@@ -15,4 +18,25 @@ class PanierController extends AbstractController
             'controller_name' => 'PanierController',
         ]);
     }
+    #[Route('/panier', name: 'app_cart')]
+public function show(UserInterface $user,PanierRepository $panierRepo,ArticleStockRepository $articleRepo):Response
+{
+    $panier = $panierRepo->findByUserID($user->getId());
+    $lignearticles = $panier->getLignePaniers();
+    $totalPrice=0;
+    $listArticle= [];
+    foreach ($lignearticles as $lignearticle)
+    {
+        $article = $articleRepo->find($lignearticle->getIdStock());
+$listArticle[] = $article;
+        $totalPrice += $article->getPrix();
+    }
+
+
+    return $this->render('panier/index.html.twig',[
+        'prix' => $totalPrice,
+        'listArticle' => $listArticle,
+
+    ]);
+}
 }
