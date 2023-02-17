04e0e5e3d132c49e997d1148d547160faa2220e1
Alex <tgvalex@hotmail.fr>
2023-02-17 11:49:38 +0100

    add function linked to the button to delete an article from the cart

--- assets/js/panier.js
+++ assets/js/panier.js
@@ -7,5 +7,11 @@ const addPanier = async () => {
 
 }
 
+const suppPanier = async () => {
+    const itemID = document.getElementById("idItem").innerText;
+    console.log(itemID);
+    const res = await fetch("del/panier?id="+itemID);
+    //document.getElementById("Result").innerHTML = await res.text();
+}
 let btnPanier = document.getElementById('addPanier');
 btnPanier.onclick = (e) => addPanier();
