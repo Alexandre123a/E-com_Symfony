6ec19ed6344d8e193e9d7eca2b3b52462051d5ff
Alex <tgvalex@hotmail.fr>
2023-02-19 22:43:04 +0100

    add function to remove item from the cart and dynamically update the cart

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,23 +1,37 @@
 
 async function addPanier(id){
     let itemID  = id;
-    console.log(itemID);
+
     const res = await fetch("add/panier?id="+itemID);
     const tab = await res.json();
-    await console.log(tab["quantity"]);
+
+
     document.getElementById("Result"+itemID).innerHTML = await tab[0];
     document.getElementById("Quantity"+itemID).innerHTML = await tab["quantity"];
+
     let totalPrice = parseInt(document.getElementById("Price").innerHTML);
     totalPrice += parseInt(tab[1]);
-    console.log(totalPrice);
+
     document.getElementById("Price").innerHTML = totalPrice;
 
 }
 
-const suppPanier = async () => {
-    console.log(itemID);
+async function suppPanier(id){
+    const itemId = id;
     const res = await fetch("del/panier?id="+itemID);
-    //document.getElementById("Result").innerHTML = await res.text();
+    const tab = await res.json();
+
+    if(tab[2])
+    {
+        document.getElementById("item"+itemID).remove();
+    }
+    document.getElementById("Result"+itemID).innerHTML = await tab[0];
+    document.getElementById("Quantity"+itemID).innerHTML = await tab["quantity"];
+
+    let totalPrice = parseInt(document.getElementById("Price").innerHTML);
+    totalPrice -= parseInt(tab[1]);
+
+    document.getElementById("Price").innerHTML = totalPrice;
 }
 
 let btnAddPanier = document.getElementsByClassName('addPanier');
@@ -27,4 +41,7 @@ for (let element of btnAddPanier)
 }
 
 let btnDelPanier = document.getElementsByClassName('delPanier');
-btnDelPanier.onclick = (e) => suppPanier();
+for (let element of btnDelPanier)
+{
+    element.onclick = (e) => suppPanier(element.id);
+}
